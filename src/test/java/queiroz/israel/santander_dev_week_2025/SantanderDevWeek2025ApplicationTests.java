3ffe11e25fd446cd84f1d843003667b277620d02
Israel Queiroz <israel.qoliveira@gmail.com>
2025-02-21 17:03:34 -0300

    Projeto Criado

---
+++ src/test/java/queiroz/israel/santander_dev_week_2025/SantanderDevWeek2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package queiroz.israel.santander_dev_week_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
